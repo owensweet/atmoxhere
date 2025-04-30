4a6e6dae43a4a84750406f4e9057c9f26f7ab0a6
owensweet <o.sweet.work@gmail.com>
2025-04-30 03:16:06 -0700

    Added firebase connection to the shop page in the lib directory. Implemented the Firestore object in page.js to get the current list of products and display them with cards. Cards still need work and different subpages for the separate collections still need to be made.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,60 @@
+'use client';
+
 import Image from "next/image";
+import { useEffect, useState } from 'react';
+import Firestore from '../lib/firebase/Firestore'
+
+
 
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+  const firestore = new Firestore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await firestore.getAllProducts();
+      setProducts(data);
+    };
+
+    fetchData();
+  }, [])
+
   return (
-    <h1 className="w-max">
-      Hello!
-    </h1>
+    <div>
+      <h1 className="text-3xl flex items-center justify-center">ATMOXHERE SHOP</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-10">
+        {products.map(product => (
+          <Card
+            key={product.id}
+            name={product.name}
+            slug={product.slug}
+            desc={product.description}
+            price={product.price}
+            stock={product.stock}
+          />
+        ))}
+      </div>
+    </div>
   );
+}
+
+function Card({ name, slug, desc, price, stock }) {
+
+  return (
+    <div className="rounded overflow-hidden shadow-md bg-green-700">
+      <div className="relative w-full h-64">
+        <Image
+          src={`/images/${slug}1.jpg`}
+          alt={name}
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="p-4">
+        <h2 className="text-lg font-semibold">{name}</h2>
+      </div>
+      <h1>$${price}$$</h1>
+      <h1>Stock: {stock}</h1>
+    </div>
+  )
 }
---
+++ src/lib/firebase/Firestore.js
@@ -0,0 +1,23 @@
+import { db } from './config';
+import { collection, getDocs, addDoc } from 'firebase/firestore'
+
+class Firestore {
+    constructor() {
+        this.productsCollection = collection(db, 'products');
+        this.ordersCollections = collection(db, 'orders');
+    }
+
+    async getAllProducts() {
+        const snapshot =  await getDocs(this.productsCollection);
+        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    }
+
+    async getAllOrders() {
+        const snapshot = await getDocs(this.ordersCollection);
+        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    }
+
+    //other crud methods add here
+}
+
+export default Firestore
