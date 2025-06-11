23ae5cdcfbd3d5fa35b49d190aff510625ec7e7a
owensweet <o.sweet.work@gmail.com>
2025-06-11 01:34:15 -0700

    Still broken, changed the fill property on the bigger image and just doubled the w and h of the smaller image

--- src/lib/components/MouseFollower.jsx
+++ src/lib/components/MouseFollower.jsx
@@ -107,21 +107,20 @@ export default function MouseFollower() {
         isTouching ? 'opacity-100' : 'opacity-0'
       }`}
     >
-        <div className='relative w-full h-full pointer-events-none'>
-            <Image
-                src={`/images/test2.png`}
-                alt="test2"
-                fill
-                className="absolute inset-0 animate-[spin_4s_linear_infinite] brightness-125 drop-shadow-[0_0_10px_rgba(0,200,90,0.6)] pointer-events-none"
-            />
-            <Image
-                src={`/images/test1.png`}
-                alt="test1"
-                width={60}
-                height={60}
-                className="relative z-10 animate-reverse-spin brightness-110 drop-shadow-[0_0_30px_rgba(0,200,80,0.3)] pointer-events-none"
-            />
-        </div>'
+        <Image
+            src={`/images/test2.png`}
+            alt="test2"
+            width={180}
+            height={180}
+            className="absolute inset-0 animate-[spin_4s_linear_infinite] brightness-125 drop-shadow-[0_0_10px_rgba(0,200,90,0.6)] pointer-events-none"
+        />
+        <Image
+            src={`/images/test1.png`}
+            alt="test1"
+            width={60}
+            height={60}
+            className="relative z-10 animate-reverse-spin brightness-110 drop-shadow-[0_0_30px_rgba(0,200,80,0.3)] pointer-events-none"
+        />
     </div>
   )
 }
