fc23c6b062360e1ec4909e0437e7a4840c5208ca
owensweet <o.sweet.work@gmail.com>
2025-08-26 00:30:44 -0700

    Increased distance of footer to rest of main content

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       <body className={`${kodeMono.className} crt overflow-x-hidden overscroll-none min-h-screen flex flex-col`}>
         <div className="scanlines"></div>
         <main className="flex-grow"> {/*dont know what flew-grow is for and if it works with the footer properly */}
-          <ClientWrapper>{children}<div className="h-52 pointer-events-none z-0" /></ClientWrapper>
+          <ClientWrapper>{children}<div className="h-152 pointer-events-none z-0" /></ClientWrapper>
           
         </main>
         <Footer />
