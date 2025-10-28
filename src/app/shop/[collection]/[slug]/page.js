8ae7712c881a776b197c3bfef45cda9cabe48e5e
owensweet <o.sweet.work@gmail.com>
2025-10-28 02:46:53 -0700

    Changed title of slug page to 'item :: data' because it looks cool

--- src/app/shop/[collection]/[slug]/page.js
+++ src/app/shop/[collection]/[slug]/page.js
@@ -75,7 +75,7 @@ function ProductInfo({ name, slug, description, price, stock, priceID }) {
   return (
     <div className="pt-0">
       <BackButton />
-      <h1 className="text-3xl flex items-center justify-center mt-0 font-extrabold py-0">ATMOXHERE SHOP</h1>
+      <h1 className="text-3xl flex items-center justify-center mt-0 font-extrabold py-0">item :: data</h1>
       <img src="/images/cyber_line.png" className="w-160 mx-auto my-6 mt-3 max-w-[90%]"/>
       <div className="flex flex-col overflow-hidden w-full h-300 text-center py-0 px-10 gap-5">
         <ImageGallery slug={slug} />
